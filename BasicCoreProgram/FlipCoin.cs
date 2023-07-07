1ed4a1c41cfcd2ad68da96d5e9c313af190901fc
sipra <sipramishra085@gmail.com>
2023-07-07 18:20:25 +0530

    [Sipra]Add:Flip coin find head or tail

---
+++ BasicCoreProgram/FlipCoin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class FlipCoin
+    {
+         
+        public void Display()
+        {
+            Random r = new Random();
+            int flip = r.Next(2);
+            Console.WriteLine(flip);
+            if (flip == 0)
+            {
+                Console.WriteLine("It's Head");
+            }
+            else
+            {
+                Console.WriteLine("It's Tail");
+            }
+
+        }
+    }
+}
+
---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Programs");
+
+            Console.WriteLine("Choose a option 1-flipcoin ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("The Flip coin Program");
+                    FlipCoin f = new FlipCoin();
+                    f.Display();
+                    break;
+                default:
+                    Console.WriteLine("Entered Wrong choice");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
