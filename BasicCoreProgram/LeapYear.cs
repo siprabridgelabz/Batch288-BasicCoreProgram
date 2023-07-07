c2523c2c5bb48c88e9c7fe9631f894e85d81911c
sipra <sipramishra085@gmail.com>
2023-07-07 18:36:39 +0530

    [Sipra]Add:Check year is leap year or not

---
+++ BasicCoreProgram/LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class LeapYear
+    {
+         
+        public void year()
+        {
+            Console.WriteLine("Please enter the year in four digit value:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("The {0} is a Leap year.", year);
+            }
+            else
+            {
+                Console.WriteLine("The {0} is not a Leap year.", year);
+            }
+        }
+    }
+}
+
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -11,8 +11,8 @@ namespace BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Programs");
-
-            Console.WriteLine("Choose a option 1-flipcoin ");
+            int n;
+            Console.WriteLine("Choose a option 1-flipcoin 2-LeadYear ");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -21,10 +21,17 @@ namespace BasicCoreProgram
                     FlipCoin f = new FlipCoin();
                     f.Display();
                     break;
+                case 2:
+                    Console.WriteLine("Lead Year");
+                    LeapYear le = new LeapYear();
+                    le.year();
+                    break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
                     break;
             }
+
+
             Console.ReadLine();
         }
     }
